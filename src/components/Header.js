cfb71958440cfe00dc46c3f0a58505e15ad435fa
Shakil <shakil@Shakils-MacBook-Air.local>
2022-03-19 07:39:06 +0530

    font size changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Header = ({ user, isAdmin }) => (
   <div className="head">
     <Image src="/clgLogo.png" width={150} height={50} />
-    <div>Tech Giants Chat Bot</div>
+    <div className='title'>ECE Techies</div>
     <div className="userInfo">
       <div>Welcome {user?.displayName || user?.email || 'Guest'}!</div>
       {isAdmin && <div className="adminText">(You are logged in as an Admin)</div>}
